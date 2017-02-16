b0ce79922cdd1403e333d26f5b2d95a5286a5ae9
Maksim P <storo1017@gmail.com>
2017-02-16 21:49:14 +0300

    Write properties in db config, create a construct method in Database class

--- vendor/meower/Database.php
+++ vendor/meower/Database.php
@@ -4,5 +4,19 @@ namespace Meower;
 
 class Database
 {
+    private $config;
 
+    private $pdo;
+
+    private $opt = [
+        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+    ];
+
+    public function __construct()
+    {
+        $this->config = require_once APP_PATH . '/config/database.php';
+        $dsn = "{$this->config['driver']}:host={$this->config['host']};dbname={$this->config['database']};charset={$this->config['charset']}";
+        $this->pdo = new \PDO($dsn, $this->config['username'], $this->config['password'], $this->opt);
+    }
 }
