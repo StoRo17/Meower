0378460bee17c93bd5aec86a12bb397487709224
Maksim P <storo1017@gmail.com>
2017-04-06 20:19:54 +0300

    The location of the initial constants now in bootstrap.php

--- public/index.php
+++ public/index.php
@@ -2,12 +2,7 @@
 
 use Meower\Application;
 
-define('ROOT', dirname(dirname(__FILE__)));
-define('APP_PATH', ROOT . '/app');
-define('FRAMEWORK_PATH', ROOT . '/vendor/meower');
-define('VIEWS_PATH', APP_PATH . '/views');
-
-require ROOT . '/bootstrap/bootstrap.php';
+require '../bootstrap/bootstrap.php';
 
 $app = new Application();
 
